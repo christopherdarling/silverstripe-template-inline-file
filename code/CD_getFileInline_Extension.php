3796420afe6a7f403dc6def230552e3af332889d
Christopher Darling <me@christopherdarling.co.uk>
2018-05-22 21:09:44 +0100

    add SS4 support

--- code/CD_getFileInline_Extension.php
+++ code/CD_getFileInline_Extension.php
@@ -2,37 +2,36 @@
 
 namespace ChristopherDarling\TemplateInlineFile;
 
-use \SSViewer;
-use \Director;
+use SilverStripe\Control\Director;
+use SilverStripe\View\TemplateGlobalProvider;
 
-class TemplateInlineFile implements \TemplateGlobalProvider
+class TemplateInlineFile implements TemplateGlobalProvider
 {
 
-  public static function get_template_global_variables()
-  {
-      return array(
-          'TemplateInlineFile' => array(
-            'method' => 'getPath',
-            'casting' => 'HTMLText'
-          )
-      );
-  }
-
-  public static function getPath($path, $pathB=null)
-  {
-    if (!is_null($pathB)) {
-      $path = $path . $pathB;
+    public static function get_template_global_variables()
+    {
+        return [
+            'TemplateInlineFile' => [
+                'method' => 'getPath',
+                'casting' => 'HTMLFragment'
+            ]
+        ];
     }
 
-    $theme = SSViewer::get_theme_folder();
-    $absPath = Director::getAbsFile($path);
+    public static function getPath($path, $pathB=null)
+    {
+        if (!is_null($pathB)) {
+            $path = $path . $pathB;
+        }
 
-    if (!file_exists($absPath)) {
-      user_error('TemplateInlineFile $path (' . $absPath  .') does not exist');
-      return;
+        $absPath = Director::getAbsFile($path);
+
+        if (!file_exists($absPath)) {
+            user_error('TemplateInlineFile $path (' . $absPath  .') does not exist');
+            return;
+        }
+
+        return file_get_contents($absPath);
     }
-
-    return file_get_contents($absPath);
-  }
 
 }
